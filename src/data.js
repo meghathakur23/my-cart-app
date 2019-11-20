024371297653ba848253c69b87bb805a8f6cddd2
megha <meghnathakur484@gmail.com>
2019-11-20 11:46:50 +0530

    second commit

--- src/data.js
+++ src/data.js
@@ -27,7 +27,7 @@ export const storeProducts = [
       id: 3,
       title: "HTC 10 - Black",
       img: "img/product-3.png",
-      price: 8,
+      price: 27,
       company: "htc",
       info:
         "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
@@ -39,11 +39,11 @@ export const storeProducts = [
       id: 4,
       title: "HTC 10 - White",
       img: "img/product-4.png",
-      price: true,
+      price: 27,
       company: "htc",
       info:
         "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-      inCart: "10",
+      inCart: false,
       count: 0,
       total: 0
     },
